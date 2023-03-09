6f6169f25a847f0e31c746ebc293ee3cc0beaaad
kszaq <737984+kszaq@users.noreply.github.com>
2023-03-09 12:09:24 +0100

    fixup

--- app/src/main/java/dev/bluehouse/enablevolte/pages/Config.kt
+++ app/src/main/java/dev/bluehouse/enablevolte/pages/Config.kt
@@ -135,7 +135,7 @@ fun Config(navController: NavController, subId: Int) {
                 true
             }
         }
-        BooleanPropertyView(label = stringResource(R.string.show_wifi_only_for_vowifi), toggled = supportWfcWifiOnly)
+        BooleanPropertyView(label = stringResource(R.string.show_wifi_only_for_vowifi), toggled = supportWfcWifiOnly) {
             supportWfcWifiOnly = if (supportWfcWifiOnly) {
                 moder.updateCarrierConfig(CarrierConfigManager.KEY_CARRIER_WFC_SUPPORTS_WIFI_ONLY_BOOL, false)
                 false
